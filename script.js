a5cb3056f7331fae7e0acacf6c1f8189dd1c3e96
oguzesguner <ohusesguner@hotmail.com>
2023-02-16 23:30:57 +0300

    Nearly done with everything. Added new js, button animations, text sizes, etc.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
 const rockButton=document.querySelector("#rockb");
 const paperButton=document.querySelector("#paperb");
 const scissorButton=document.querySelector("#scissorb");
@@ -7,26 +8,27 @@ let playerScore=0;
 let round=0;
 //
 //
+
 let playerDecision;
     rockButton.addEventListener("click",function(){
         document.getElementById("player").innerHTML=document.getElementById("player").innerHTML+"Rock"+"</br>";
         playerDecision="rock";
-        game(pcSelect(),playerDecision);
+        game(pcSelect(),playerDecision); 
     });
     paperButton.addEventListener("click",function(){
         document.getElementById("player").innerHTML=document.getElementById("player").innerHTML+"Paper"+"</br>";
         playerDecision="paper";
-        game(pcSelect(),playerDecision);
+        game(pcSelect(),playerDecision); 
     });
     scissorButton.addEventListener("click",function(){
         document.getElementById("player").innerHTML=document.getElementById("player").innerHTML+"Scissors"+"</br>";
         playerDecision="scissor";
-        game(pcSelect(),playerDecision);
+        game(pcSelect(),playerDecision); 
     });
 function pcSelect(){
     let pcDecision;
     let options=["rock","paper","scissors"]
-    let randomNumber=Math.floor(Math.random()*3);
+    let randomNumber=rNumber(3);
     if(options[randomNumber]==="rock")
     {
         pcDecision="rock";
@@ -48,6 +50,7 @@ function pcSelect(){
 
 function game(comp,player){
     round+=1;
+    document.getElementById("roundCount").innerHTML="Round "+round;
     if((comp=="rock" && player=="rock")||(comp=="paper" && player=="paper")||(comp=="scissor" && player=="scissor"))
     {
         document.getElementById("game").innerHTML=document.getElementById("game").innerHTML+"It's a tie!"+"</br>";
@@ -64,6 +67,8 @@ function game(comp,player){
         playerScore+=1;
         document.getElementById("score").innerHTML="Computer = "+pcScore+" - "+playerScore+" = Player";
     }
+    roundCheck();
+    return playerScore,pcScore,round;
 }
 
 resetButton.addEventListener("click",function(){
@@ -74,6 +79,51 @@ resetButton.addEventListener("click",function(){
     document.getElementById("computer").innerHTML="";
     document.getElementById("game").innerHTML="";
     document.getElementById("player").innerHTML="";
-
-
+    document.getElementById("roundCount").innerHTML="Round "+round;
+    rockButton.disabled=false;
+    paperButton.disabled=false;
+    scissorButton.disabled=false;
 })
+function roundCheck(){
+    if(playerScore==5 && pcScore<5)
+{
+        document.getElementById("game").innerHTML=document.getElementById("game").innerHTML+"Game Over!</br>You Won!!!"
+        rockButton.disabled=true;
+        paperButton.disabled=true;
+        scissorButton.disabled=true;
+        document.getElementById("message").innerHTML="Congratulations! You've beaten the computer "+playerScore+" to "+pcScore+" Well done!!</br> Press the button to play again!!"
+}
+    else if(pcScore==5 && playerScore<5)
+{
+        document.getElementById("game").innerHTML=document.getElementById("game").innerHTML+"Game Over!</br>The Computer Won!!!"
+        rockButton.disabled=true;
+        paperButton.disabled=true;
+        scissorButton.disabled=true;
+        document.getElementById("message").innerHTML="Better luck next time! The computer has emerged victorious with a score of "+pcScore+" to "+playerScore+"</br> Press the button to play again!!"
+
+}
+else if(playerScore>pcScore)
+{
+    let winning=
+    ["Congratulations! You're making excellent progress towards victory!",
+    "You're really dominating the game - keep it up!",
+    "You're on fire! Your moves are unstoppable"];
+    document.getElementById("message").innerHTML=winning[rNumber(winning.length)];
+}
+else if(pcScore>playerScore)
+    {
+        let losing=[
+        "Don't worry, you can still make a comeback!",
+        "Things might be looking tough now, but remember: it's not over until it's over.",
+        "Sometimes you lose, but that's okay - it's all part of the game. Just keep trying!"];
+        document.getElementById("message").innerHTML=losing[rNumber(losing.length)]   
+    }
+
+} 
+
+function rNumber(max)
+{
+    return Math.floor(Math.random()*max);
+}
+
+});

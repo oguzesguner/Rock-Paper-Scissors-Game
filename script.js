445a7b74e715622ba3ae0adeaff7d12547d20b08
oguzesguner <ohusesguner@hotmail.com>
2023-02-17 00:00:55 +0300

    Mostly finished. Added extra lines to be written to the player. Added two small flags for language switching. Need to incorporate that later. Added clicking animation to all clickable elements.

--- script.js
+++ script.js
@@ -80,6 +80,7 @@ resetButton.addEventListener("click",function(){
     document.getElementById("game").innerHTML="";
     document.getElementById("player").innerHTML="";
     document.getElementById("roundCount").innerHTML="Round "+round;
+    document.getElementById("message").innerHTML="Welcome!";
     rockButton.disabled=false;
     paperButton.disabled=false;
     scissorButton.disabled=false;
@@ -107,16 +108,43 @@ else if(playerScore>pcScore)
     let winning=
     ["Congratulations! You're making excellent progress towards victory!",
     "You're really dominating the game - keep it up!",
-    "You're on fire! Your moves are unstoppable"];
+    "You're on fire! Your moves are unstoppable",
+    "You're outplaying the computer at every turn - keep it up!",
+    "You're making all the right moves - the computer doesn't stand a chance!",
+    "Victory is within your grasp - just a few more wins to go!",
+    "You're proving yourself to be a master strategist - the computer is no match for you!",
+    "You're playing brilliantly - the computer is on the ropes!",
+    "You're on a roll - keep it up and you'll be the champion in no time"];
     document.getElementById("message").innerHTML=winning[rNumber(winning.length)];
 }
 else if(pcScore>playerScore)
     {
-        let losing=[
-        "Don't worry, you can still make a comeback!",
-        "Things might be looking tough now, but remember: it's not over until it's over.",
-        "Sometimes you lose, but that's okay - it's all part of the game. Just keep trying!"];
-        document.getElementById("message").innerHTML=losing[rNumber(losing.length)]   
+    let losing=
+    ["Don't worry, you can still make a comeback!",
+    "Things might be looking tough now, but remember: it's not over until it's over.",
+    "Sometimes you lose, but that's okay - it's all part of the game. Just keep trying!",
+    "It's a tough game, but you can bounce back from this setback!",
+    "You're facing some adversity, but don't give up - you've got this!",
+    "The computer might be ahead for now, but you can turn things around with some smart moves!",
+    "You're facing a worthy opponent, but remember: you're a worthy player, too!",
+    "You're in a bit of a hole, but keep your head up - there's still a chance to win!",
+    "You're not quite in the lead yet, but with a bit of persistence you can come out on top!",];
+    document.getElementById("message").innerHTML=losing[rNumber(losing.length)];   
+    }
+    else if(playerScore===pcScore)
+    {
+    let tie=
+    ["It's a closely contested game - every move counts!",
+    "You're evenly matched with the computer - this is going to be a nail-biter!",
+    "It's neck and neck - who will make the first mistake?",
+    "Wow, what a battle! You and the computer are both equally skilled.",
+    "You and the computer seem to be in a deadlock. Who will break it first?",
+    "A tie game! You and the computer are so evenly matched, it's hard to tell who will win in the end.",
+    "You and the computer are neck-and-neck. It's anyone's game at this point!",
+    "It's a draw! The tension is building - who will make the first mistake?",
+    "A tie! The perfect opportunity to reflect on your strategy.",];
+    document.getElementById("message").innerHTML=tie[rNumber(tie.length)];   
+
     }
 
 } 
